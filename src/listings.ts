caa98ddf42d70966e61b786bc894dd134539ecd0
chathuranga <chathuranga@freshworks.io>
2022-06-08 12:03:22 +0530

    withdraw instructions added

--- src/listings.ts
+++ src/listings.ts
@@ -9,6 +9,11 @@ import {
 import { Wallet } from "@metaplex/js";
 import { Client } from "./client";
 import { AuctionHouse, Nft, Listing, Creator } from "./types";
+import {
+  WithdrawFromTreasuryInstructionAccounts,
+  WithdrawFromTreasuryInstructionArgs,
+} from "@metaplex-foundation/mpl-auction-house/dist/src/generated/instructions";
+import BN from "bn.js";
 
 const { instructions } = AuctionHouseProgram;
 
@@ -21,6 +26,7 @@ const {
   createPrintPurchaseReceiptInstruction,
   createCancelInstruction,
   createCancelListingReceiptInstruction,
+  createWithdrawFromTreasuryInstruction,
 } = instructions;
 
 export interface PostListingParams {
@@ -375,6 +381,54 @@ export class ListingsClient extends Client {
         })
       )
       .add(printPurchaseReceiptInstruction);
+
+    txt.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    txt.feePayer = publicKey;
+
+    const signed = await signTransaction(txt);
+
+    const signature = await connection.sendRawTransaction(signed.serialize());
+
+    await connection.confirmTransaction(signature, "confirmed");
+  }
+
+  async withdrawFromTreasury(amount: BN) {
+    const { publicKey, signTransaction } = this.wallet;
+    const connection = this.connection;
+    const ah = this.auctionHouse;
+
+    const auctionHouse = new PublicKey(ah.address);
+    const authority = new PublicKey(ah.authority);
+    const auctionHouseFeeAccount = new PublicKey(ah.auctionHouseFeeAccount);
+    const treasuryMint = new PublicKey(ah.treasuryMint);
+    const auctionHouseTreasury = new PublicKey(ah.auctionHouseTreasury);
+    const treasuryWithdrawalDestination = new PublicKey(
+      ah.treasuryWithdrawalDestination
+    );
+
+    const withdrawFromTreasuryInstructionAccounts: WithdrawFromTreasuryInstructionAccounts =
+      {
+        treasuryMint: treasuryMint,
+        authority: authority,
+        treasuryWithdrawalDestination: treasuryWithdrawalDestination,
+        auctionHouseTreasury: auctionHouseTreasury,
+        auctionHouse: auctionHouse,
+      };
+
+    const withdrawFromTreasuryInstructionArgs: WithdrawFromTreasuryInstructionArgs =
+      {
+        amount: amount,
+      };
+
+    const withdrawFromTreasuryInstruction =
+      createWithdrawFromTreasuryInstruction(
+        withdrawFromTreasuryInstructionAccounts,
+        withdrawFromTreasuryInstructionArgs
+      );
+
+    const txt = new Transaction();
+
+    txt.add(withdrawFromTreasuryInstruction);
 
     txt.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
     txt.feePayer = publicKey;
